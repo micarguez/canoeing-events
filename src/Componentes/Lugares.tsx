a7936dd8fbe2b02cc6f87daa82ba53c6788e86c1
Micaela Rodriguez <micadecolonia@gmail.com>
2023-02-27 14:07:32 -0300

    added filter radio buttons

--- src/Componentes/Lugares.tsx
+++ src/Componentes/Lugares.tsx
@@ -3,9 +3,12 @@ import './Lugares.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { checkHasToken } from '../utils';
-import { fetchLugares, fetchLugarPorNombre, fetchLugarPorNombreYDesc } from '../api';
+import { fetchLugares, fetchLugarPorNombreYDesc, fetchLugarPorTipoAguas, fetchLugarPorUsuario } from '../api';
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
+
 function Lugares() {
-  const [lugares, setLugares] = useState([])
+  
+const [lugares, setLugares] = useState([])
 
 useEffect(() => {
   fetchLugares().then((data: any) => setLugares(data));
@@ -13,6 +16,13 @@ useEffect(() => {
 
 const handleChange = (event: { target: { value: any; }; }) => {
   fetchLugarPorNombreYDesc(event.target.value).then((data: any) => setLugares(data));
+};
+
+const handleChangeRadioButton = (event: { target: { value: any; }; }) => {
+
+  let subs = event.target.value.split(' ')
+
+  fetchLugarPorTipoAguas(subs[1]).then((data: any) => setLugares(data));
 };
 
 const redirectToLugar = (id: any) => {
@@ -26,13 +36,31 @@ if(!checkHasToken()){
 
   return (
     <>
-      <div className="searchInput_Container">
-        <input
-        className="search"
-        placeholder="Busca un lugar..." 
-        onChange={handleChange}
-        />
+    <div className="wrapper">
+      <div className="filters">
+        <div className="searchInput_Container">
+          <input
+          className="search"
+          placeholder="Busca un lugar..." 
+          onChange={handleChange}
+          />
+        </div>
+        <div className="filtro_aguas">
+        <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Tipo de aguas</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="Aguas tranquilas"
+              name="radio-buttons-group"
+              onChange={handleChangeRadioButton}
+            >
+              <FormControlLabel value="Aguas tranquilas" control={<Radio />} label="Aguas tranquilas"/>
+              <FormControlLabel value="Aguas bravas" control={<Radio />} label="Aguas bravas"/>
+            </RadioGroup>
+          </FormControl>
+        </div>
       </div>
+      
       
       <div className='App container'>
         {lugares?.map((lugar: any) => (
@@ -49,7 +77,9 @@ if(!checkHasToken()){
             </Card.Body>
           </Card><br /></>
         ))}
-      </div></>
+      </div>
+      </div>
+      </>
   );
 }
 
