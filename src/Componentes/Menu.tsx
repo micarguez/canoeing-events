de008f8f8ddcea411ed8477146e1fdd89c20b5e7
Micaela Rodriguez <micadecolonia@gmail.com>
2023-02-27 14:07:37 -0300

    new menu

--- src/Componentes/Menu.tsx
+++ src/Componentes/Menu.tsx
@@ -17,16 +17,20 @@ function Menu() {
     <>
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="/">My App</Navbar.Brand>
+                <Navbar.Brand href="/">Canotaje 2023</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" ></Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link className="menuButtons" href="/"> Home </Nav.Link>
-                        <Nav.Link className="menuButtons" href="/lugares"> Lugares </Nav.Link>
-                        <NavDropdown title="Mas opciones" id="basic-nav-dropdown">
-                            <NavDropdown.Item className="menuButtons" href="/eventos"> Eventos </NavDropdown.Item>
-                            {(Boolean(token) && <NavDropdown.Item onClick={onLogout}> Logout </NavDropdown.Item>)}
+                        <NavDropdown title="Lugares" id="basic-nav-dropdown">
+                            <NavDropdown.Item className="menuButtons" href="/lugares"> Crear lugar </NavDropdown.Item>
+                            <NavDropdown.Item className="menuButtons" href="/lugares"> Ver lugares </NavDropdown.Item>
                         </NavDropdown>
+                        <NavDropdown title="Eventos" id="basic-nav-dropdown">
+                            <NavDropdown.Item className="menuButtons" href="/eventos"> Crear evento </NavDropdown.Item>
+                            <NavDropdown.Item className="menuButtons" href="/eventos"> Ver eventos </NavDropdown.Item>
+                        </NavDropdown>
+                        {(Boolean(token) && <Nav.Link className="menuButtons" onClick={onLogout}> Logout </Nav.Link>)}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
