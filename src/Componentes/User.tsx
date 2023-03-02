16e18cfaf89d6dbeb2c48752faaa89da2c9709d1
Micaela Rodriguez <micadecolonia@gmail.com>
2023-03-02 18:48:14 -0300

    update imports

--- src/Componentes/User.tsx
+++ src/Componentes/User.tsx
@@ -4,13 +4,11 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { checkHasToken, redirectToLugares } from '../utils';
-import { fetchLugarPorUsuario, fetchUsuario } from '../api';
+import { checkHasToken } from '../utils';
+import { fetchUsuario } from '../api';
 import { useParams } from 'react-router-dom';
 import ImageOne from './../img/user.jpg';
-import setLugares from './Lugares';
 
 function User() {
   const [user, setUser] = useState<any>(null);
