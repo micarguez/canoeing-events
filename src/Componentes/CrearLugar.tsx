10bff318acd4667f6db1dc11db88e895f96d0b9a
Micaela Rodriguez <micadecolonia@gmail.com>
2023-03-08 12:20:22 -0300

    fixed tipos

--- src/Componentes/CrearLugar.tsx
+++ src/Componentes/CrearLugar.tsx
@@ -3,27 +3,38 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./CrearLugar.css";
 import { crearLugar } from "../api";
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
 
 const CrearLugar = () => {
   const [nombre, setNombre] = useState("");
   const [descripcion, setDescripcion] = useState("");
-  const [tipo, setTipo] = useState("");
+  const [tipo, setTipo] = useState("1");
   const [ubicacion, setUbicacion] = useState("");
   const [imagen_url, setImagen_url] = useState("");
 
   let token = localStorage.getItem('token');
   let user = localStorage.getItem('user_id');
 
- const handleSubmit = async (e: any) => {
-  e.preventDefault();
-  crearLugar(nombre, descripcion, tipo, ubicacion, imagen_url, user, token);
-  
-  setNombre("");
-  setDescripcion("");
-  setTipo("");
-  setUbicacion("");
-  setImagen_url("");
-};
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    crearLugar(nombre, descripcion, tipo, ubicacion, imagen_url, user, token);
+    
+    setNombre("");
+    setDescripcion("");
+    setTipo("1");
+    setUbicacion("");
+    setImagen_url("");
+   };
+
+   const handleChangeRadioButton = (event: { target: { value: any; }; }) => {
+    let subs = event.target.value.split(' ')
+    if(subs[1] == "bravas"){
+        setTipo("1");
+    }else if(subs[1] == "tranquilas"){
+        setTipo("2");
+    }
+    
+  };
 
   return (
     <><h1 id="form-title">Crear un nuevo lugar</h1>
@@ -54,14 +65,20 @@ const CrearLugar = () => {
                   onChange={(e: any) => setDescripcion(e.target.value)} />
               <br />
               <br />
-              <TextField
-                  label="Tipo"
-                  type="string"
-                  autoComplete="false"
-                  value={tipo}
-                  onChange={(e: any) => setTipo(e.target.value)} />
-              <br />
-              <br />
+              <div className="radio_aguas">
+                <FormControl>
+                    <FormLabel id="demo-radio-buttons-group-label">Tipo de aguas</FormLabel>
+                    <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="Aguas bravas"
+                    name="radio-buttons-group"
+                    onChange={handleChangeRadioButton}
+                    >
+                    <FormControlLabel value="Aguas bravas" control={<Radio />} label="Aguas bravas"/>
+                    <FormControlLabel value="Aguas tranquilas" control={<Radio />} label="Aguas tranquilas"/>
+                    </RadioGroup>
+                </FormControl>
+            </div>
               <TextField
                   label="Ubicacion"
                   type="string"
